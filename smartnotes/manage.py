8afaaaa97659b45691929608b9275d032d84d2e3
Saddam Ahmed <sa522408@gmail.com>
2025-01-22 16:02:51 +0530

    Update manage.py
    added app for vercel to use

--- smartnotes/manage.py
+++ smartnotes/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
 
 def main():
@@ -20,3 +21,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+app = get_wsgi_application()
